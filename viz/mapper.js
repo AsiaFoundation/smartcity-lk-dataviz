3f6af3f25bb32d0702dc23f279c27aa31987fca4
Nick Doiron <nicholas.doiron@asiafoundation.org>
2016-02-25 10:53:21 -0800

    color-coding GN Division Map by percent-satisfaction

--- viz/mapper.js
+++ viz/mapper.js
@@ -1,5 +1,7 @@
-var map, selectLayer;
+var map, selectLayer, selectFeature;
 var happinessFor = {};
+var boundsFor = {};
+var layerFor = {};
 
 $(function() {
   map = L.map('map', {
@@ -8,22 +10,59 @@ $(function() {
   .setView([9.67, 80.15], 13);
   L.tileLayer('//tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(map);
 
+  function standardStyle(feature) {
+    var happy = happinessFor[feature.properties.GND_C];
+    var fillColor = '#FF0000';
+    if (happy >= 40) {
+      fillColor = '#F6C600';
+      if (happy >= 60) {
+        fillColor = '#60B044';
+      }
+    }
+    return {
+      color: '#000',
+      weight: 1,
+      opacity: 1,
+      fillColor: fillColor,
+      fillOpacity: 0.4
+    };
+  }
+
+  function makeClick (feature, layer) {
+    var bounds = boundsFor[feature.properties.GND_C];
+    map.fitBounds(L.latLngBounds(
+      L.latLng(bounds[1], bounds[0]),
+      L.latLng(bounds[3], bounds[2])
+    ));
+
+    selectLayer && selectLayer.setStyle(standardStyle(selectFeature));
+    layer.setStyle({ fillOpacity: 0.8 });
+    selectLayer = layer;
+    selectFeature = feature;
+
+    $("#gn-name").text(feature.properties.GND_N);
+    $("#gn-detail").text('Overall satisfaction: ' + happinessFor[feature.properties.GND_C] + '%');
+  }
+
   $.get('viz/data/center.geojson', function (gj) {
     if (typeof gj === 'string') {
       gj = JSON.parse(gj);
     }
-    for (var f = 0; f < gj.features.length; f++) {
+    $.each(gj.features, function (f) {
       var myIcon = L.divIcon({className: 'divlabel label-' + f});
 
       var feature = gj.features[f];
       happinessFor[feature.properties.GND_C] = feature.properties.happiness;
 
-      L.marker({
+      var label = L.marker({
         lat: feature.geometry.coordinates[1],
         lng: feature.geometry.coordinates[0]
       }, { icon: myIcon }).addTo(map);
+      label.on('click', function (e) {
+        makeClick(feature, layerFor[feature.properties.GND_C]);
+      });
       $(".label-" + f).text(feature.properties.happiness + '%');
-    }
+    });
   });
 
   $.get('viz/data/chavakachcheriya.geojson', function (gj) {
@@ -31,33 +70,16 @@ $(function() {
       gj = JSON.parse(gj);
     }
     for (var f = 0; f < gj.features.length; f++) {
-      gj.features[f].properties.bounds = makeBounds(gj.features[f].geometry.coordinates);
+      var feature = gj.features[f];
+      boundsFor[feature.properties.GND_C] = makeBounds(feature.geometry.coordinates);
     }
 
     L.geoJson(gj, {
-      style: function(feature) {
-        return {
-          color: '#000',
-          weight: 0.9,
-          opacity: 1,
-          fillColor: '#aaccff',
-          fillOpacity: 0.5
-        }
-      },
+      style: standardStyle,
       onEachFeature: function(feature, layer) {
+        layerFor[feature.properties.GND_C] = layer;
         layer.on('click', function (e) {
-          var bounds = feature.properties.bounds;
-          map.fitBounds(L.latLngBounds(
-            L.latLng(bounds[1], bounds[0]),
-            L.latLng(bounds[3], bounds[2])
-          ));
-
-          selectLayer && selectLayer.setStyle({ fillOpacity: 0.5 });
-          layer.setStyle({ fillOpacity: 1 });
-          selectLayer = layer;
-
-          $("#gn-name").text(feature.properties.GND_N);
-          $("#gn-detail").text('Overall satisfaction: ' + happinessFor[feature.properties.GND_C] + '%');
+          makeClick(feature, layer);
         });
       }
     }).addTo(map);
